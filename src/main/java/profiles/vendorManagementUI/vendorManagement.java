de78942d7360f039576e7bc43c23138e87414372
Ian_McNulty <gi5631@wayne.edu>
2021-03-08 18:37:38 -0500

    update gitlab

--- src/main/java/profiles/vendorManagementUI/vendorManagement.java
+++ src/main/java/profiles/vendorManagementUI/vendorManagement.java
@@ -3,8 +3,6 @@
             -Need to restrict based on datatype
             -This problem needs to be fixed in vendorRegistration
     7.  The system shall allow owner or purchaser users to delete any vendor profiles only when their balance is 0.
-    8.  The system shall allow owner or purchaser users to search for a vendor profile
-        by either vendor id or vendor full name.
     12. When deleting a vendor profile, the system shall warn the users that all associated purchase
         orders will be deleted. If the users accept deletion, then the system shall automatically delete
         all purchase orders associated with the deleted vendor.
